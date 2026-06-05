56cc134fefc8ecc70f587191a2c984e8e7effb54
Evgenii Petrov <epoy74@gmail.com>
2026-06-05 23:43:11 +0500

    feat(core) add IStringCompressor for compressing/deconpressing data

---
+++ src/CSharpTestTasks.Core/Compression/Abstractions/IStringCompressor.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpTestTasks.Core.Compression.Abstractions;
+
+// контракт алгоритма компрессии / декомпрессии
+public interface IStringCompressor
+{
+    string Compress(string input);
+    string Decompress(string input);
+}
